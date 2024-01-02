3d7eed999fd234643961c0304250b4f91a7d375a
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-01-02 19:57:11 -0300

    Otimizando a lógica do cookies

--- src/player.py
+++ src/player.py
@@ -1,4 +1,6 @@
 
+from time import time
+
 from selenium.webdriver.common.by import By
 
 from product import Product
@@ -9,12 +11,14 @@ class Player:
 
     # TODO: arrumar para prefixo dos valores do cookies, por exemplo, million e etc
     # TODO: comprar os upgrades
-    # TODO: fazer uma logica do cookies mais eficiente para ser possivel fazer mais clicks por segundo
 
     def __init__(self, driver):
         self.__driver = driver
         
         self.__big_cookie = None
+        self.__cookies = 0
+        self.__seconds = 5
+        self.__last_update = time()
 
         self.__products_prefix = "product"
         self.__products_price_prefix = "productPrice"
@@ -31,10 +35,17 @@ class Player:
     def update(self):
         self.__big_cookie.click()
 
+        current_time = time()
+
+        if current_time - self.__last_update > self.__seconds:
+            self.__cookies = int(find_element(self.__driver, (By.ID, "cookies")).text.split()[0])
+            self.__last_update = current_time
+        
         for i in range(0, 20):
 
-            if self.__products[i].get_price() and self.__products[i].get_price() < int(find_element(self.__driver, (By.ID, "cookies")).text.split()[0]):
+            if self.__products[i].get_price() and self.__products[i].get_price() < self.__cookies:
                 self.__products[i].buy()
+                self.__cookies -= self.__products[i].get_price()
                 
                 if i != 19 and not self.__products[i + 1].get_price():
                     self.__products[i + 1].update_price()
