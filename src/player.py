d6af1388316457903fc4d4a4b76150f7d3c707ec
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-01-02 17:58:25 -0300

    Já está comprando o item da loja, porém está muito lerdo

--- src/player.py
+++ src/player.py
@@ -8,17 +8,23 @@ class Player:
 
     def __init__(self, driver) -> None:
         self.__driver = driver
-        self.__cookie = ElementFinder.find(self.__driver, (By.ID, "bigCookie"), 10)
 
     def update(self):
-        self.__cookie.click()
+        cookie = ElementFinder.find(self.__driver, (By.ID, "bigCookie"), 10)
+        cookie.click()
 
         products_prefix = "product"
         products_price_prefix = "productPrice"
-        end = 4
 
         # Talvez chamar so depois de alguns segundos para não ficar consumindo processamento
-        for i in range(0, 4):
-            product_price = ElementFinder.find(self.__driver, (By.ID, products_price_prefix + str(i)))
 
-            print(product_price.text)
+        for i in range(0, 19):
+            cookies = int(ElementFinder.find(self.__driver, (By.ID, "cookies")).text.split()[0])
+            product_price = ElementFinder.find(self.__driver, (By.ID, products_price_prefix + str(i)))
+            
+            if not product_price.text:
+                break
+            
+            if int(product_price.text.replace(",", "")) < cookies:
+                ElementFinder.find(self.__driver, (By.ID, products_prefix + str(i))).click()
+                break
